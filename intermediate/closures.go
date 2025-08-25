b3ed2cfd645be606d47dce01777c38940e245af2
Minstrel Nwachukwu <minstreln@gmail.com>
2025-08-25 17:22:53 +0100

    intermediate start - closures

---
+++ intermediate/closures.go
@@ -0,0 +1,34 @@
+package basics
+
+import "fmt"
+
+func main() {
+
+	sequence := adder()
+	fmt.Println(sequence())
+
+	subtractor := func() func(int) int {
+		countdown := 99
+		return func(x int) int {
+			countdown -= x
+			return countdown
+		}
+	}()
+
+	// using the closure subtractor
+	fmt.Println(subtractor(1))
+	fmt.Println(subtractor(2))
+	fmt.Println(subtractor(3))
+	fmt.Println(subtractor(4))
+	fmt.Println(subtractor(5))
+}
+
+func adder() func() int {
+	i := 0
+	fmt.Println("Previous value of i:", i)
+	return func() int {
+		i++
+		fmt.Println("adding 1 to i:")
+		return i
+	}
+}
