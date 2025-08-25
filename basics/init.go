01bb58a97baed72e589a7c55e17cc552c64c34b6
Minstrel Nwachukwu <minstreln@gmail.com>
2025-08-25 12:37:00 +0100

    init

---
+++ basics/init.go
@@ -0,0 +1,28 @@
+package basics
+
+import "fmt"
+
+func init() {
+	// init function is called before main function
+	// it is used to initialize variables or state
+	println("Init function called to initialize the program")
+}
+
+func init() {
+	// init function is called before main function
+	// it is used to initialize variables or state
+	println("Init function called to initialize the program2")
+}
+
+func init() {
+	// init function is called before main function
+	// it is used to initialize variables or state
+	println("Init function called to initialize the program3")
+}
+
+func main() {
+
+	// init function has not parameters and return values
+
+	fmt.Println("inside main function")
+}
